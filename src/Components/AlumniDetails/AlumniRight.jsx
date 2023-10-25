a31fed2f335178e59e8b5cff7e3fee9f4fa74f4b
ayush ranjan <ayushranjan656@gmail.com>
2023-10-26 01:09:26 +0530

    refrence pages usestate added

--- src/Components/AlumniDetails/AlumniRight.jsx
+++ src/Components/AlumniDetails/AlumniRight.jsx
@@ -1,66 +1,110 @@
 import '../../style/facultyalumni.css'
 import '../../style/MainContainer.css'
 export default function AlumniRight() {
+  // Define state variables to store the input values
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [branch, setBranch] = useState('');
+  const [batch, setBatch] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [jobProfile, setJobProfile] = useState('');
+
+  // Event handler to update the state when input values change
+  const handleInputChange = (e, stateUpdater) => {
+    stateUpdater(e.target.value);
+  };
+
+  // Event handler for form submission (you can add your logic here)
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission action
+    // You can add your code to process the form data here
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Branch:', branch);
+    console.log('Batch:', batch);
+    console.log('Phone Number:', phoneNumber);
+    console.log('Job Profile:', jobProfile);
+  };
+
   return (
     <div>
       <div className="r">
         <h1 className="rheading">Enter Alumni Details</h1>
       </div>
-      <div className="row input">
-        <div className="col-md-6 col-sm-12">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="First Name"
-          />
-        </div>
-        <div className="col-md-6 col-sm-12">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="Last Name"
-          />
-        </div>
-      </div>
-      <div className="row input">
-        <div className="col-md-6 col-sm-12">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="Branch"
-          />
-        </div>
-        <div className="col-md-6 col-sm-12">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="Batch"
-          />
-        </div>
-      </div>
-      <div className="row input">
-        <div className="col-md-3 col-sm-12">
-          <div className="input-group">
-            <input type="text" className="form-control" value="+91" readOnly />
+      <form onSubmit={handleSubmit}>
+        <div className="row input">
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => handleInputChange(e, setFirstName)}
+            />
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => handleInputChange(e, setLastName)}
+            />
           </div>
         </div>
-        <div className="col-md-9 col-sm-12">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Phone Number"
-          />
+        <div className="row input">
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Branch"
+              value={branch}
+              onChange={(e) => handleInputChange(e, setBranch)}
+            />
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Batch"
+              value={batch}
+              onChange={(e) => handleInputChange(e, setBatch)}
+            />
+          </div>
         </div>
-      </div>
-      
-      <div className="row input">
-        <div className="col-12">
-          <input type="text" className="form-control" placeholder="Current Job Profile" />
+        <div className="row input">
+          <div className="col-md-3 col-sm-12">
+            <div className="input-group">
+              <input type="text" className="form-control" value="+91" readOnly />
+            </div>
+          </div>
+          <div className="col-md-9 col-sm-12">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => handleInputChange(e, setPhoneNumber)}
+            />
+          </div>
         </div>
-      </div>
-      <div className="mt-auto justify-content-end d-flex">
-        <button type="submit" className="btn btn-primary btn-lg rounded register-btn">Register</button>
-      </div>
+        <div className="row input">
+          <div className="col-12">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Current Job Profile"
+              value={jobProfile}
+              onChange={(e) => handleInputChange(e, setJobProfile)}
+            />
+          </div>
+        </div>
+        <div className="mt-auto justify-content-end d-flex">
+          <button type="submit" className="btn btn-primary btn-lg rounded register-btn">
+            Register
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/Components/StudentDetails/StudentRight.jsx
+++ src/Components/StudentDetails/StudentRight.jsx
@@ -1,61 +1,95 @@
+import { useState } from 'react';
 import '../../style/facultyalumni.css'
 export default function StudentRight() {
+  // Define state variables to store the input values
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [rollNumber, setRollNumber] = useState('');
+  const [branch, setBranch] = useState('');
+
+  // Event handler to update the state when input values change
+  const handleInputChange = (e, stateUpdater) => {
+    stateUpdater(e.target.value);
+  };
+
+  // Event handler for form submission (you can add your logic here)
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission action
+    // You can add your code to process the form data here
+  };
+
   return (
     <div>
       <div className="r">
         <h1 className="rheading">Enter Student Details</h1>
       </div>
-      <div className="row input">
-        <div className="col-md-6 col-sm-12">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="First Name"
-          />
-        </div>
-        <div className="col-md-6 col-sm-12">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="Last Name"
-          />
-        </div>
-      </div>
-      <div className="row input">
-        <div className="col-md-3 col-sm-12">
-          <div className="input-group">
-            <input type="text" className="form-control" value="+91" readOnly />
+      <form onSubmit={handleSubmit}>
+        <div className="row input">
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => handleInputChange(e, setFirstName)}
+            />
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => handleInputChange(e, setLastName)}
+            />
           </div>
         </div>
-        <div className="col-md-9 col-sm-12">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Phone Number"
-          />
+        <div className="row input">
+          <div className="col-md-3 col-sm-12">
+            <div className="input-group">
+              <input type="text" className="form-control" value="+91" readOnly />
+            </div>
+          </div>
+          <div className="col-md-9 col-sm-12">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => handleInputChange(e, setPhoneNumber)}
+            />
+          </div>
         </div>
-      </div>
-      <div className="row input">
-        <div className="col-12">
-          <input type="text" className="form-control" placeholder="Roll Number" />
+        <div className="row input">
+          <div className="col-12">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Roll Number"
+              value={rollNumber}
+              onChange={(e) => handleInputChange(e, setRollNumber)}
+            />
+          </div>
         </div>
-      </div>
-      <div className="row input">
-        <div className="col-12">
-          <input type="email" className="form-control" placeholder="Branch" />
+        <div className="row input">
+          <div className="col-12">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Branch"
+              value={branch}
+              onChange={(e) => handleInputChange(e, setBranch)}
+            />
+          </div>
         </div>
-      </div>
 
-      {/* <button
-        type="button"
-        // className="form-control btn btn-primary btn-sm rounded"
-        className="btn btn-primary btn-lg rounded register-btn"
-      >
-        Register
-      </button> */}
-      <div className="mt-auto justify-content-end d-flex">
-        <button className="btn btn-primary btn-lg rounded register-btn">Register</button>
-      </div>
+        <div className="mt-auto justify-content-end d-flex">
+          <button type="submit" className="btn btn-primary btn-lg rounded register-btn">
+            Register
+          </button>
+        </div>
+      </form>
     </div>
   );
 }

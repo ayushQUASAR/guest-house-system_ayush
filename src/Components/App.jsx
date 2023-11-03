2464b2056ceba39573205ae89996b69c4129f3b9
Kunal-0919 <kunalbishnoi003@gmail.com>
2023-11-04 00:26:39 +0530

    [Feat] Added calender component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,45 +1,42 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../Home';
-import Login from "./Login"
-import Dash from './Dash';
-import Registration from './Registration'
-import Alumni from './AlumniDetails/AlumniMain'
-import Student from './StudentDetails/StudentMain'
-import Faculty from './FacultyDetails/FacultyMain'
-import AdminRegistration from './AdminRegistration';
-import { UserContextProvider } from '../Components/ContextHooks/UserContext';
-import AdminRegistrationMain from './AdminRegistrationMain';
-import UserDetail from './UserDetail';
-import { LoginContextProvider } from './ContextHooks/LoginContext';
-import  Container  from './BookingForm/Container';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../Home";
+import Login from "./Login";
+import Dash from "./Dash";
+import Registration from "./Registration";
+import Alumni from "./AlumniDetails/AlumniMain";
+import Student from "./StudentDetails/StudentMain";
+import Faculty from "./FacultyDetails/FacultyMain";
+import AdminRegistration from "./AdminRegistration";
+import { UserContextProvider } from "../Components/ContextHooks/UserContext";
+import AdminRegistrationMain from "./AdminRegistrationMain";
+import UserDetail from "./UserDetail";
+import { LoginContextProvider } from "./ContextHooks/LoginContext";
+import Container from "./BookingForm/Container";
 
 function App() {
   return (
     <>
-   
       <Router>
         <LoginContextProvider>
-      <UserContextProvider>
-        <Routes>
-        
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/Booking" element={<Container/>} />
-          
-          <Route path="/Register" element={<Registration/>} />
-          <Route path="/AdminRegistration" element={<AdminRegistrationMain/>} />
-          <Route path="/Faculty" element={<Faculty/>} />
-          <Route path="/Student" element={<Student/>} />
-          <Route path="/Alumni" element={<Alumni/>} />
-          <Route path="/Dashboard" element={<Dash/>} />
-          <Route path="/UserDetails" element={<UserDetail/>} />
-          <Route path="/" element={<Login/>} />
-        </Routes>
-        </UserContextProvider>
+          <UserContextProvider>
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/Booking" element={<Container />} />
+              <Route path="/Register" element={<Registration />} />
+              <Route
+                path="/AdminRegistration"
+                element={<AdminRegistrationMain />}
+              />
+              <Route path="/Faculty" element={<Faculty />} />
+              <Route path="/Student" element={<Student />} />
+              <Route path="/Alumni" element={<Alumni />} />
+              <Route path="/Dashboard" element={<Dash />} />
+              <Route path="/UserDetails" element={<UserDetail />} />
+              <Route path="/" element={<Login />} />
+            </Routes>
+          </UserContextProvider>
         </LoginContextProvider>
       </Router>
-      
     </>
   );
 }
---
+++ src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+import "./Calendar.css";
+import RoomDetailsModal from "./Modal";
+import "./Modal.css";
+
+const Calendar = () => {
+  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const roomDetails = [
+    {
+      guestHouseName: "Main Guest House",
+      acCount: 15,
+      nonACCount: 8,
+      location: "Near Girls Hostel",
+      numOfBeds: 1,
+    },
+    {
+      guestHouseName: "MBH Guest House",
+      acCount: 20,
+      nonACCount: 8,
+      location: "Near Girls Hostel",
+      numOfBeds: 1,
+    },
+    {
+      guestHouseName: "Snackers",
+      acCount: 20,
+      nonACCount: 8,
+      location: "Near Girls Hostel",
+      numOfBeds: 1,
+    },
+  ];
+
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const getDaysInMonth = () => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstDayOfWeek = new Date(year, month, 1).getDay();
+
+    const calendar = [];
+
+    for (let i = 0; i < firstDayOfWeek; i++) {
+      calendar.push(null);
+    }
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      calendar.push(day);
+    }
+
+    return calendar;
+  };
+
+  const isCurrentMonth = () => {
+    const today = new Date();
+    return (
+      date.getFullYear() === today.getFullYear() &&
+      date.getMonth() === today.getMonth()
+    );
+  };
+
+  const prevMonth = () => {
+    if (isCurrentMonth()) {
+      return;
+    }
+    setDate(new Date(date.getFullYear(), date.getMonth() - 1));
+  };
+
+  const nextMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1));
+  };
+
+  const handleDateClick = (day) => {
+    if (day !== null) {
+      setSelectedDate(day);
+      setIsModalOpen(true);
+    }
+  };
+
+  const renderCalendarRows = () => {
+    const calendarData = getDaysInMonth();
+    const rows = [];
+    const today = new Date().getDate();
+
+    for (let i = 0; i < calendarData.length; i += 7) {
+      const row = calendarData.slice(i, i + 7);
+      rows.push(
+        <tr key={i}>
+          {row.map((day, index) => {
+            if (day === null || (isCurrentMonth() && day < today)) {
+              return (
+                <td key={index} className="calendar-cell empty-cell">
+                  {/* Empty cell */}
+                </td>
+              );
+            }
+            return (
+              <td
+                key={index}
+                className={`calendar-cell ${
+                  day === today && isCurrentMonth() ? "current-day" : ""
+                }`}
+                onClick={() => handleDateClick(day)}
+              >
+                <div
+                  className="div-td"
+                  id={
+                    day
+                      ? day.toLocaleString("en-US", {
+                          day: "numeric",
+                          month: "short",
+                          year: "numeric",
+                        })
+                      : ""
+                  }
+                >
+                  {day !== null && (
+                    <>
+                      <h3 className="curdate-calendar">{day}</h3>
+                      <span className="acCount">AC : {15}</span>
+                      <span className="nonCount">Non-AC : {8}</span>
+                    </>
+                  )}
+                </div>
+              </td>
+            );
+          })}
+        </tr>
+      );
+    }
+
+    return rows;
+  };
+
+  return (
+    <div id="calendar-root">
+      <div className="header">
+        <div
+          className={`btn ${isCurrentMonth() ? "disabled" : ""}`}
+          onClick={prevMonth}
+        >
+          Previous Month
+        </div>
+        <h2 className="month-year">
+          {date.toLocaleString("default", { month: "long", year: "numeric" })}
+        </h2>
+        <div className="btn" onClick={nextMonth}>
+          Next Month
+        </div>
+      </div>
+      <table className="calendar-table">
+        <thead>
+          <tr>
+            {daysOfWeek.map((day) => (
+              <th key={day} className="day-of-week">
+                {day}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{renderCalendarRows()}</tbody>
+      </table>
+
+      <RoomDetailsModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        roomDetails={roomDetails}
+      />
+    </div>
+  );
+};
+
+export default Calendar;

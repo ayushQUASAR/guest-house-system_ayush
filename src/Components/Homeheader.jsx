4017d43546d16dd679f5fdedaf9d97d0e183ff72
Kunal-0919 <kunalbishnoi003@gmail.com>
2023-11-23 00:58:39 +0530

    myfiles

--- src/Components/Homeheader.jsx
+++ src/Components/Homeheader.jsx
@@ -1,98 +1,79 @@
-import React, {useState, useEffect} from 'react'
-import { useLoginContext } from './ContextHooks/LoginContext';
-import "../style/dashNav.css"
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import logo from "../images/logo_250.png.png"
-import { NavLink } from 'react-router-dom';
-import LocalDiningRoundedIcon from '@mui/icons-material/LocalDiningRounded';
-import ContactlessIcon from '@mui/icons-material/Contactless';
-import AlignHorizontalLeftOutlinedIcon from '@mui/icons-material/AlignHorizontalLeftOutlined';
-import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import React, { useState, useEffect } from "react";
+import { useLoginContext } from "./ContextHooks/LoginContext";
+import "../style/dashNav.css";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import logo from "../images/logo_250.png.png";
+import { NavLink } from "react-router-dom";
+import LocalDiningRoundedIcon from "@mui/icons-material/LocalDiningRounded";
+import ContactlessIcon from "@mui/icons-material/Contactless";
+import AlignHorizontalLeftOutlinedIcon from "@mui/icons-material/AlignHorizontalLeftOutlined";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 import { Link } from "react-scroll";
-import HelpCenterIcon from '@mui/icons-material/HelpCenter';
+import HelpCenterIcon from "@mui/icons-material/HelpCenter";
 
 const HomeHeader = () => {
-    const { isLogged } = useLoginContext();
-    const [isAdmin, setIsAdmin] = useState(false);
+  const { isLogged } = useLoginContext();
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}/check-session`, {
-          method: 'GET',
-          credentials: 'include',
-        })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.loggedIn) {
-            setIsAdmin(data.isAdmin); 
-          }
-        });
-      }, []);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/check-session`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.loggedIn) {
+          setIsAdmin(data.isAdmin);
+        }
+      });
+  }, []);
 
-    return (
-        <>
-        <div>
-            <div className="h-nav">
+  return (
+    <>
+      <div>
+        <div className="h-nav">
+          <div className="nav-wrapper">
+            <div className="colg-name">
+              National Institute Of Technology Jalandhar
+            </div>
+            <div className="h-navlogo">
+              <img src={logo} alt="NIT_logo" />
+            </div>
+            <div className="bg-nav">
+              <li>
+                <Link to="/" smooth={true} duration={50}>
+                  <div className="nav-optn">
+                    <HomeRoundedIcon color="white" />
+                    <div className="optn-name">Home</div>{" "}
+                  </div>
+                </Link>
 
-                <div className="nav-wrapper">
-                    <div className='colg-name'>National Institute Of Technology Jalandhar</div>
-                    <div className="h-navlogo">
-                        <img src={logo} alt="NIT_logo" />
-                    </div>
-                    <div className="bg-nav">
-                        <li>
+                <Link to="contactHeader" smooth={true} duration={100}>
+                  <div className="nav-optn">
+                    <ContactlessIcon />
 
-                            <Link
-                                to="/"
-                                smooth={true}
-                                duration={50}
-                            >
+                    <div className="optn-name"> ContactUs</div>
+                  </div>
+                </Link>
+                <div className="nav-optn">
+                  <LocalDiningRoundedIcon /> Food
+                </div>
+                <Link to="guidelinesHeader" smooth={true} duration={50}>
+                  <div className="nav-optn">
+                    <AlignHorizontalLeftOutlinedIcon />
+                    <div className="optn-name"> Guidelines</div>
+                  </div>
+                </Link>
 
-                                <div className="nav-optn"><HomeRoundedIcon color="white" /><div className='optn-name'>Home</div>       </div>
-                            </Link>
-
-
-                            <Link
-                                to="contactHeader"
-                                smooth={true}
-                                duration={100}
-
-                            >
-                                <div className="nav-optn">
-                                    <ContactlessIcon /> 
-                                    
-                                 <div className="optn-name"> ContactUs</div>  
-                                </div>
-                            </Link>
-                            <div className="nav-optn">
-                                <LocalDiningRoundedIcon />  Food
-                            </div>
-                            <Link
-                                to="guidelinesHeader"
-                                smooth={true}
-                                duration={50}
-
-                            >
-                                <div className="nav-optn">
-                                    <AlignHorizontalLeftOutlinedIcon />  
-                                    <div className="optn-name"> Guidelines</div> 
-                                </div>
-                            </Link>
-
-                            <Link
-                                to="AboutUs"
-                                smooth={true}
-                                duration={50}
-
-                            >
-
-                                <div className="nav-optn">
-                                    <HelpCenterIcon />  <div className="optn-name">About</div> 
-                                </div>
-                            </Link>
-                            {/* <div className="nav-optn">
+                <Link to="AboutUs" smooth={true} duration={50}>
+                  <div className="nav-optn">
+                    <HelpCenterIcon /> <div className="optn-name">About</div>
+                  </div>
+                </Link>
+                {/* <div className="nav-optn">
                                 <CurrencyRupeeIcon /> Charges
                             </div> */}
-                            {/* <Link
+                {/* <Link
                                 to="Facility"
                                 smooth={true}
                                 duration={50}
@@ -101,7 +82,7 @@ const HomeHeader = () => {
                                 <div className="nav-optn">
                                     Facility</div>
                             </Link> */}
-                            {/* <Link
+                {/* <Link
                                 to="Availability"
                                 smooth={true}
                                 duration={100}
@@ -110,37 +91,40 @@ const HomeHeader = () => {
 
                                 Availability
                             </Link> */}
-                            {!isAdmin && (<NavLink to="/UserDetails">
+                {!isAdmin && (
+                  <NavLink to="/UserDetails">
+                    <div className="nav-optn">Profile</div>
+                  </NavLink>
+                )}
+                {!isLogged && (
+                  <NavLink to="/login">
+                    <div className="nav-optn">Login</div>
+                  </NavLink>
+                )}
 
-                                <div className="nav-optn">Profile</div>
-                            </NavLink>)} 
-                            {!isLogged && (<NavLink to="/login">
+                {isLogged && isAdmin && (
+                  <NavLink to="/login">
+                    <div className="nav-optn">Dashboard</div>
+                  </NavLink>
+                )}
 
-                                <div className="nav-optn">Login</div>
-                            </NavLink>)}
-
-                            {isLogged && isAdmin && (<NavLink to="/login">
-
-                                <div className="nav-optn">Dashboard</div>
-                            </NavLink>)}
-
-                            {/* {isLogged && !isAdmin && (<NavLink to="/login">
+                {/* {isLogged && !isAdmin && (<NavLink to="/login">
 
                                 <div className="nav-optn">Book Now</div>
                             </NavLink>)} */}
 
-                            {/* <NavLink to="/Booking">
+                {/* <NavLink to="/Booking">
                             <div className="nav-optn" id="BookNow">
                                 Book Now
                             </div>
                             </NavLink> */}
-                        </li>
-                    </div>
-                </div>
+              </li>
             </div>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default HomeHeader
+export default HomeHeader;

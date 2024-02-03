2a3748c22ecf14cbf74459aa6c05c51b3b7f30b4
Kshitij Agarwal <kshitija024@gmail.com>
2024-02-03 14:51:35 +0530

    Pop up issue fixed

--- src/Components/BOOKING/BookingComponent.jsx
+++ src/Components/BOOKING/BookingComponent.jsx
@@ -1,26 +1,17 @@
-
-
-import React, { useState, useEffect } from 'react';
-import './BookingComponent.css';
-import MyComponent from './MyComponent';
-import Popup from '../PopUp/Popup';
-import './MyComponent.css';
-import ApproveBooking from '../BookingApproval/ApproveBooking.jsx'
+import React, { useState, useEffect } from "react";
+import "./BookingComponent.css";
+import MyComponent from "./MyComponent";
+import Popup from "../PopUp/Popup";
+import BookingApprovalPopup from "./BookingApprovalPopup.jsx"
+import "./MyComponent.css";
+import ApproveBooking from "../BookingApproval/ApproveBooking.jsx";
 
 const BookingComponent = (pram) => {
+  const [messageHead_m, setMessagehead] = useState("");
 
-
-
-
-
-  const [messageHead_m, setMessagehead] = useState('')
-
-  const [para1_m, setPara1] = useState('')
-  const [para2_m, setPara2] = useState('')
-  const [popup, setPopup] = useState(false)
-
-
-
+  const [para1_m, setPara1] = useState("");
+  const [para2_m, setPara2] = useState("");
+  const [popup, setPopup] = useState(false);
 
   const guesthouseno = pram.guesthouseno;
   const rooms = pram.rooms;
@@ -31,35 +22,33 @@ const BookingComponent = (pram) => {
   const startDate = pram.startDate;
   const endDate = pram.endDate;
 
-  console.log('this is pram', pram)
+  console.log("this is pram", pram);
 
   useEffect(() => {
-    const ghh1 = document.getElementById('gh1');
-    const ghh2 = document.getElementById('gh2');
-    const ghh3 = document.getElementById('gh3');
+    const ghh1 = document.getElementById("gh1");
+    const ghh2 = document.getElementById("gh2");
+    const ghh3 = document.getElementById("gh3");
 
     console.log("inside use effect");
 
     if (guesthouseno === 1) {
-      ghh1.style.backgroundColor = 'black';
+      ghh1.style.backgroundColor = "black";
     }
     if (guesthouseno === 2) {
-      ghh2.style.backgroundColor = 'black';
+      ghh2.style.backgroundColor = "black";
     }
     if (guesthouseno === 3) {
-      ghh3.style.backgroundColor = 'black';
+      ghh3.style.backgroundColor = "black";
     }
 
     // Clean-up function (optional)
     return () => {
       // Reset styles or perform any clean-up when the component unmounts
-      ghh1.style.backgroundColor = '';
-      ghh2.style.backgroundColor = '';
-      ghh3.style.backgroundColor = '';
+      ghh1.style.backgroundColor = "";
+      ghh2.style.backgroundColor = "";
+      ghh3.style.backgroundColor = "";
     };
   }, [guesthouseno]);
-
-
 
   const [selectedGuestHouse, setSelectedGuestHouse] = useState(guesthouseno);
   const [selectedRooms, setSelectedRooms] = useState([]);
@@ -71,9 +60,6 @@ const BookingComponent = (pram) => {
     setData(newData);
   };
 
-
-
-
   const handleChange = (event) => {
     setSelectedDeadlineValue(Number(event.target.value));
   };
@@ -84,20 +70,16 @@ const BookingComponent = (pram) => {
 
   const closePopup = () => {
     setPopupOpen(false);
-  }
+  };
   const handleApproval = () => {
-
     if (data === rooms) {
-
       const data = {
         booking: id,
-        status: 'accept',
+        status: "accept",
         guestHouseAllotted: selectedGuestHouse,
         roomsAllotted: selectedRooms,
-        paymentDeadline: 24
+        paymentDeadline: 24,
       };
-
-
 
       console.log("Accept Body :", data);
       fetch(`${import.meta.env.VITE_API_URL}/admin/bookingApproval`, {
@@ -105,70 +87,66 @@ const BookingComponent = (pram) => {
         mode: "cors",
         body: JSON.stringify(data),
         headers: {
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
       })
         .then((res) => res.json())
         // .then((data) => console.log(data))
         .then((data) => {
-
-
-          setMessagehead('Booking Successful')
-          console.log()
+          setMessagehead("Booking Successfully Approved");
+          console.log();
           setPopup(true);
-          console.log(data)
-
+          console.log(data);
         })
 
-
-        .catch((err) => console.error(err.message))
+        .catch((err) => console.error(err.message));
       // openPopup();
     } else {
-
-     
-      window.alert("Please Select the required no of rooms.")
-     
+      window.alert("Please Select the required no of rooms.");
     }
-
-
-
-  }
+  };
 
   const handleRooms = (x) => {
     setSelectedRooms(x);
-  }
-
-
+  };
 
   return (
     <div className="mai">
-
-      <span onClick={handleBack} style={{ fontSize: "13px", cursor: "pointer", position: "absolute", color: "white", borderRadius: "4px", backgroundColor: "#0073cf", marginLeft: "2px", marginTop: "2px", padding: "2px" }}>
+      <span
+        onClick={handleBack}
+        style={{
+          fontSize: "13px",
+          cursor: "pointer",
+          position: "absolute",
+          color: "white",
+          borderRadius: "4px",
+          backgroundColor: "#0073cf",
+          marginLeft: "2px",
+          marginTop: "2px",
+          padding: "2px",
+        }}
+      >
         Back
       </span>
       <div className="head1">
-
-        <div>
-          Booking Rooms
-        </div>
+        <div>Booking Rooms</div>
       </div>
       <div className="main1">
         <div>
-
           <p className="para">Select Guest House</p>
         </div>
 
         <div className="flex-containerTY">
-          <div id="gh1"  >Main Guest House </div>
-          <div id="gh2"  >Mega Guest House </div>
-          <div id="gh3" >   SAC Guest House</div>
+          <div id="gh1">Main Guest House </div>
+          <div id="gh2">Mega Guest House </div>
+          <div id="gh3"> SAC Guest House</div>
         </div>
 
-
-
         <div className="new">
-
-          <div> Select {rooms}  {rooms === 1 ? "room" : "rooms"} </div>
+          <div>
+            {" "}
+            Select {rooms} {rooms === 1 ? "room" : "rooms"}{" "}
+          </div>
           <div className="roombooking">
             <div id="available"></div>Available
             <div id="selected"></div>Selected
@@ -176,32 +154,58 @@ const BookingComponent = (pram) => {
           </div>
         </div>
 
-
-        {
-          selectedGuestHouse === 1 ?
-            <div style={{ marginTop: '20px' }} id="sacg1">
-              Main Guest House  
-                <MyComponent onDataChange={handleDataChange} setRooms={handleRooms} guesthouseid={guesthouseno} userStartDate={startDate} userEndDate={endDate} maxRooms={rooms} n={10} />
-            </div>
-            : selectedGuestHouse === 2 ?
-              <div style={{ marginTop: '10px' }} id="sacg2">
-                     Mega Guest House 
-                <MyComponent onDataChange={handleDataChange} setRooms={handleRooms} guesthouseid={guesthouseno} userStartDate={startDate} userEndDate={endDate} maxRooms={rooms} n={12} />
-               
+        {selectedGuestHouse === 1 ? (
+          <div style={{ marginTop: "20px" }} id="sacg1">
+            Main Guest House
+            <MyComponent
+              onDataChange={handleDataChange}
+              setRooms={handleRooms}
+              guesthouseid={guesthouseno}
+              userStartDate={startDate}
+              userEndDate={endDate}
+              maxRooms={rooms}
+              n={10}
+            />
           </div>
-              : <div style={{ marginTop: '10px' }} id="sacg3">
+        ) : selectedGuestHouse === 2 ? (
+          <div style={{ marginTop: "10px" }} id="sacg2">
+            Mega Guest House
+            <MyComponent
+              onDataChange={handleDataChange}
+              setRooms={handleRooms}
+              guesthouseid={guesthouseno}
+              userStartDate={startDate}
+              userEndDate={endDate}
+              maxRooms={rooms}
+              n={12}
+            />
+          </div>
+        ) : (
+          <div style={{ marginTop: "10px" }} id="sacg3">
             SAC Guest House
-              <MyComponent onDataChange={handleDataChange} setRooms={handleRooms} guesthouseid={guesthouseno} userStartDate={startDate} userEndDate={endDate} maxRooms={rooms} n={8} />
-              </div>
-        }
+            <MyComponent
+              onDataChange={handleDataChange}
+              setRooms={handleRooms}
+              guesthouseid={guesthouseno}
+              userStartDate={startDate}
+              userEndDate={endDate}
+              maxRooms={rooms}
+              n={8}
+            />
+          </div>
+        )}
 
-
-
-        <div className='bookButtons'>
-          <div className="book" style={{ cursor: "pointer" }} onClick={handleApproval}>Book Now</div>
+        <div className="bookButtons">
+          <div
+            className="book"
+            style={{ cursor: "pointer" }}
+            onClick={handleApproval}
+          >
+            Book Now
+          </div>
           {/* <div className="dropdown"> */}
-     
-            {/* <select style={{ position: 'relative', right: "5rem", bottom: "6px" }} onChange={handleChange}>
+
+          {/* <select style={{ position: 'relative', right: "5rem", bottom: "6px" }} onChange={handleChange}>
               <option value="24">
                 24 hrs
               </option>
@@ -216,7 +220,14 @@ const BookingComponent = (pram) => {
             </select> */}
           {/* </div> */}
         </div>
-        {popup && <Popup setPopup={setPopup} messageHead={messageHead_m} para1={para1_m} para2={para2_m} />}
+        {popup && (
+          <BookingApprovalPopup
+            setPopup={setPopup}
+            messageHead={messageHead_m}
+            para1={para1_m}
+            para2={para2_m}
+          />
+        )}
         {/* {isPopupOpen && <Popup isOpen={isPopupOpen} onClose={closePopup} messageHead={'Booked Successfully'} para1={''} para2={'Confirmation mail has been sent to User.'} />} */}
       </div>
     </div>
@@ -224,4 +235,3 @@ const BookingComponent = (pram) => {
 };
 
 export default BookingComponent;
-
--- src/Components/BookingApproval/ApproveTable.jsx
+++ src/Components/BookingApproval/ApproveTable.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 // import  "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 // import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import "./Approvetable.css"
+import "./Approvetable.css";
 import BookingComponent from "../BOOKING/BookingComponent";
 import CustomPrompt from "../PopUp/Rejectpopup";
-
 
 const Approvaltable = ({ onSecondPage }) => {
   const [pendingBooking, setPendingBooking] = useState(null);
@@ -12,19 +11,16 @@ const Approvaltable = ({ onSecondPage }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [isPopupOpen, setPopupOpen] = useState(false);
   const [isCustomPromptOpen, setCustomPromptOpen] = useState(false);
-  const [customPromptReason, setCustomPromptReason] = useState('');
+  const [customPromptReason, setCustomPromptReason] = useState("");
   const [dialog, setDialog] = useState(false);
-
 
   const toggleDialog = () => {
     setDialog((prev) => !prev);
-  }
-
+  };
 
   // const openPopup = () => {
   //   setPopupOpen(true);
   // };
-
 
   // const closePopup = () => {
   //   setPopupOpen(false);
@@ -38,169 +34,184 @@ const Approvaltable = ({ onSecondPage }) => {
   };
 
   const handleReject = (id) => {
-    const confirm =  window.confirm(`Are you sure you want to reject this booking?`);
+    const confirm = window.confirm(
+      `Are you sure you want to reject this booking?`
+    );
     if (confirm === true) {
       setCurrentUser(id);
       openCustomPrompt();
     }
-    
   };
 
   const handlePromptSubmit = (reason) => {
     if (reason === "") {
-      window.alert('Rejection not processed. Reason was not provided.');
+      window.alert("Rejection not processed. Reason was not provided.");
     } else {
       setCustomPromptReason(reason);
-      handleApproval(currentUser, 'reject', reason);
+      handleApproval(currentUser, "reject", reason);
       closeCustomPrompt();
     }
   };
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/booking/approved/pending`)
       .then((res) => res.json())
-      .then((data) => { setPendingBooking(data); console.log(data) })
+      .then((data) => {
+        setPendingBooking(data);
+        console.log(data);
+      })
       .then((err) => console.log(err));
   }, []);
 
   const handleBack = () => {
     setIsFirstPage(true);
-    console.log('clicked');
-  }
- 
+    console.log("clicked");
+  };
 
-  const handleSubmit = () => {
-
-  }
-  const handleApproval = (id, status,reason) => {
-    const confirm =  window.confirm(`Are you sure you want to ${status} this booking?`);
-
+  const handleSubmit = () => {};
+  const handleApproval = (id, status, reason) => {
+    const confirm = window.confirm(
+      `Are you sure you want to ${status} this booking?`
+    );
 
     if (confirm === true) {
-    if (status === 'accept') {
-      setIsFirstPage(false);
-      // openPopup();
-      onSecondPage();
-    }
+      if (status === "accept") {
+        setIsFirstPage(false);
+        // openPopup();
+        onSecondPage();
+      }
 
-    if (status === 'reject') {
-      const rejectBody = {
-        booking: id,
-        status: "reject",
-        reason:reason,
-      };
+      if (status === "reject") {
+        const rejectBody = {
+          booking: id,
+          status: "reject",
+          reason: reason,
+        };
 
-      console.log("reject body: ", rejectBody);
-      fetch(`${import.meta.env.VITE_API_URL}/admin/bookingApproval`, {
-        method: "POST",
-        mode: "cors",
-        body: JSON.stringify(rejectBody),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          console.log(data.message);
-
-          setPendingBooking((prev) => {
-            const selectedUsers = prev.filter((user) => user._id !== id);
-            return selectedUsers;
-          });
-
+        console.log("reject body: ", rejectBody);
+        fetch(`${import.meta.env.VITE_API_URL}/admin/bookingApproval`, {
+          method: "POST",
+          mode: "cors",
+          body: JSON.stringify(rejectBody),
+          headers: {
+            "Content-Type": "application/json",
+          },
         })
-        .catch((err) => console.log(err));
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            console.log(data.message);
 
+            setPendingBooking((prev) => {
+              const selectedUsers = prev.filter((user) => user._id !== id);
+              return selectedUsers;
+            });
+          })
+          .catch((err) => console.log(err));
+      }
     }
-  }
-}
-
-
-
-
+  };
 
   return (
     <>
-    {
-      isFirstPage ? 
-      <div className="approval-table">
-      <div className="d-flex flex-row justify-content-between">
-      
-      <table className="book-approval-table">
-        <thead>
-          <tr>
-            <th>S.No</th>
-            <th>Name</th>
-            <th>Guest House</th>
-            <th>No. of Rooms</th>
-            <th>Email</th>
-            <th>Contact No.</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Reason of Booking</th>
-           <th>Govt/College ID</th>
-            <th>Approval</th>
-            </tr>
-        </thead>
-        <tbody>
-    {pendingBooking &&
-      pendingBooking.length > 0 &&
-      pendingBooking.map((user, index) => (
-        <tr key={user._id}>
-          <td>{index + 1}</td>
-          <td>{user.name}</td>
-          <td>{user.guestHouseSelected}</td>
-          <td>{user.roomsSelected}</td>
-          <td>{user.email}</td>
-          <td>{user.phone}</td>
-          <td>{new Date(user.startDate).toLocaleDateString()}</td>
-  <td>{new Date(user.endDate).toLocaleDateString()}</td>
-          <td>{user.purpose}</td>
-          {
-            user.roomBooker.isAdmin ? <td>-</td> : 
-            <td>
-            <button className="popup-button" onClick={toggleDialog}>View</button>
+      {isFirstPage ? (
+        <div className="approval-table">
+          <div className="d-flex flex-row justify-content-between">
+            <table className="book-approval-table">
+              <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Name</th>
+                  <th>Guest House</th>
+                  <th>No. of Rooms</th>
+                  <th>Email</th>
+                  <th>Contact No.</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                  <th>Reason of Booking</th>
+                  <th>Govt/College ID</th>
+                  <th>Approval</th>
+                </tr>
+              </thead>
+              <tbody>
+                {pendingBooking &&
+                  pendingBooking.length > 0 &&
+                  pendingBooking.map((user, index) => (
+                    <tr key={user._id}>
+                      <td>{index + 1}</td>
+                      <td>{user.name}</td>
+                      <td>{user.guestHouseSelected}</td>
+                      <td>{user.roomsSelected}</td>
+                      <td>{user.email}</td>
+                      <td>{user.phone}</td>
+                      <td>{new Date(user.startDate).toLocaleDateString()}</td>
+                      <td>{new Date(user.endDate).toLocaleDateString()}</td>
+                      <td>{user.purpose}</td>
+                      {user.roomBooker.isAdmin ? (
+                        <td>-</td>
+                      ) : (
+                        <td>
+                          <button
+                            className="popup-button"
+                            onClick={toggleDialog}
+                          >
+                            View
+                          </button>
                           {dialog && (
-                              <div className="dialog">
+                            <div className="dialog">
                               <div className="dialog-content">
-                                  <button className="close-icon" onClick={toggleDialog}>&#10005;</button>
-                                  <img className="popup-image" src={user.roomBooker.idProof} alt="Popup Image" />
+                                <button
+                                  className="close-icon"
+                                  onClick={toggleDialog}
+                                >
+                                  &#10005;
+                                </button>
+                                <img
+                                  className="popup-image"
+                                  src={user.roomBooker.idProof}
+                                  alt="Popup Image"
+                                />
                               </div>
-                              </div>
+                            </div>
                           )}
-            </td>
-          }
-          
-          
-          <td>
-            <button
-              type="button"
-              className="btn btn-success btn-sm mr-3"
-              onClick={() => {
-                setCurrentUser(user);
-                handleApproval(user._id, 'accept');
-              }}
-            >
-              Accept
-            </button>
-            <button
-              type="button"
-              className="btn btn-danger btn-sm"
-              onClick={() => handleReject(user._id, 'reject')}
-            >
-              Reject
-            </button>
-          </td>
-        </tr>
-      ))}
-  </tbody>
-        </table>
-      </div>
-
-         
-        </div> : <BookingComponent  updateBooking={setPendingBooking} guesthouseno={currentUser.guestHouseSelected} id={currentUser._id} rooms={currentUser.roomsSelected} startDate={currentUser.startDate} endDate={currentUser.endDate} onBack={handleBack} />
-      }
-       <CustomPrompt
+                        </td>
+                      )}
+                      <td>
+                        <button
+                          type="button"
+                          className="btn btn-success btn-sm mr-3"
+                          onClick={() => {
+                            setCurrentUser(user);
+                            handleApproval(user._id, "accept");
+                          }}
+                        >
+                          Accept
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-danger btn-sm"
+                          onClick={() => handleReject(user._id, "reject")}
+                        >
+                          Reject
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        <BookingComponent
+          updateBooking={setPendingBooking}
+          guesthouseno={currentUser.guestHouseSelected}
+          id={currentUser._id}
+          rooms={currentUser.roomsSelected}
+          startDate={currentUser.startDate}
+          endDate={currentUser.endDate}
+          onBack={handleBack}
+        />
+      )}
+      <CustomPrompt
         isOpen={isCustomPromptOpen}
         onClose={closeCustomPrompt}
         onSubmit={handlePromptSubmit}

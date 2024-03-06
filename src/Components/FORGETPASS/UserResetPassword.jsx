18413d5beb6349924be4b43871ce54fb8debc004
ABHISHEK KUMAR <100033618+abhishek110803@users.noreply.github.com>
2024-03-06 19:35:07 +0530

    Added old password box

--- src/Components/FORGETPASS/UserResetPassword.jsx
+++ src/Components/FORGETPASS/UserResetPassword.jsx
@@ -1,15 +1,16 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import './ResetPassword.css';
 import { useUserContext } from "../ContextHooks/UserContext";
 
 
 const UserResetPassword = () => {
   const [pass, setPass] = useState("");
+  const [oldpass, setOldpass] = useState("");
   const [confirmPass, setConfirmPass] = useState("");
   const [email, setEmail] = useState("");
   const [error, setError] = useState(false);
   const { userId } = useUserContext();
-  console.log("ResetPassword: "+userId);
+  console.log("ResetPassword: " + userId);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/users/${userId}`)
@@ -28,34 +29,37 @@ const UserResetPassword = () => {
       });
   }, [userId]);
 
-useEffect(()=> {
+  useEffect(() => {
 
-if(confirmPass!== "" && pass !== confirmPass) {
-  setError(true);
-}
-else if(confirmPass!== "" && pass === confirmPass) {
-  setError(false);
-}
+    if (confirmPass !== "" && pass !== confirmPass) {
+      setError(true);
+    }
+    else if (confirmPass !== "" && pass === confirmPass) {
+      setError(false);
+    }
 
-}, [pass,confirmPass]);
+  }, [pass, confirmPass]);
 
 
-const handleClick = ()=> {
-  fetch(`${import.meta.env.VITE_API_URL}/login/update-password`, {
+  const handleClick = () => {
+    fetch(`${import.meta.env.VITE_API_URL}/login/update-password`, {
       method: "POST",
       mode: "cors",
+      
       body: JSON.stringify({
         email,
         password: pass,
+        oldpass,
       }),
       headers: {
         "Content-Type": "application/json"
       }
-  })
-  .then((res) => res.json())
-  .then((data) => window.alert(data.message))
-  .catch((err) => console.log(err.message));
-}
+
+    })
+      .then((res) => res.json())
+      .then((data) => window.alert(data.message))
+      .catch((err) => console.log(err.message));
+  }
 
   return (
     <div className="resetmain1">
@@ -63,17 +67,21 @@ const handleClick = ()=> {
 
       <div>
         <form className="resetform1">
-        <label htmlFor="registeredemail">Enter Registered Email:</label>
-          <br className="resetbreaker" />
-          <input type="text" id="registeredemail" name="emailid" className="forgetinputbox" onChange={(e) => {setEmail(e.target.value)}} value={email} disabled/>
+          <label htmlFor="registeredemail">Enter Registered Email:</label>
+          {/* <br className="resetbreaker" /> */}
+          &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; <input type="text" id="registeredemail" name="emailid" className="" onChange={(e) => { setEmail(e.target.value) }} value={email} disabled /><br /><br />
+          <label htmlFor="oldpassword">Enter Old Password :</label>
+          {/* <br className="resetbreaker" /> */}
+          &nbsp;<input type="password" id="oldpass" name="oldpass" className="resetinputbox" onChange={(e) => { setOldpass(e.target.value) }} /><br /><br />
+
           <label htmlFor="newpassword">Enter New Password :</label>
           <br className="resetbreaker" />
-          <input type="password" id="newpass" name="newpass" className="resetinputbox"  onChange={(e) => {setPass(e.target.value)}}/><br /><br />
+          <input type="password" id="newpass" name="newpass" className="resetinputbox" onChange={(e) => { setPass(e.target.value) }} /><br /><br />
           <label htmlFor="cnfrmpassword">Confirm New Password:</label>
           <br className="resetbreaker" />
-          <input type="password" id="cnfpass" name="confirmpass" className="resetinputbox1" onChange={(e)=> {setConfirmPass(e.target.value)}} />
-          <br/>
-         <p style={{color: "red", fontSize:"0.8rem", textDecoration:"underline"}}>{error && "Both passwords does not match..."}</p> 
+          <input type="password" id="cnfpass" name="confirmpass" className="resetinputbox1" onChange={(e) => { setConfirmPass(e.target.value) }} />
+          <br />
+          <p style={{ color: "red", fontSize: "0.8rem", textDecoration: "underline" }}>{error && "Both passwords does not match..."}</p>
         </form>
       </div>
       <div className="resetbook" onClick={handleClick}>Submit</div>
